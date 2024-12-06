236073f9da257b791b2e301f49a5f311fe76fef8
Totto16 <tobiausgais@gmail.com>
2024-12-06 12:25:48 +0100

    feat: add day 06

---
+++ src/06/index.ts
@@ -0,0 +1,4 @@
+const createRoute6 = <Route extends string|number>(author: string, route: Route):Route => {
+	console.log(`[createRoute] route created by ${author} at ${Date.now()}`);
+	return route
+  }
