53bc58cb4fba2233ae1130835a0d3fc8dcf02928
Totto16 <tobiausgais@gmail.com>
2024-12-27 14:46:03 +0100

    fix:  a few small fixes in the bash script

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-## no +e 
-set +x
+## no -e 
+set -x
 
-DIRECTORIES="$(find src -maxdepth 1 -mindepth 1 -type d)"
+DIRECTORIES="$(find src -maxdepth 1 -mindepth 1 -type d | sort -n)"
 
 for DIRECTORY in $DIRECTORIES; do
     npx tsc -p "$DIRECTORY"
